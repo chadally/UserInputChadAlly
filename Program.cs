dbd688f616777564033c87129163afd33a188f94
Chad Ally <chadally@usf.edu>
2020-01-31 18:50:18 -0500

    Added string greeting

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace UserInputChadAlly
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, this is Chad Ally");
+            Console.WriteLine("Say hello world and hit enter.");
+            string greeting;
+            greeting = Console.ReadLine();
+            Console.WriteLine("You typed: " + greeting);
         }
     }
 }
